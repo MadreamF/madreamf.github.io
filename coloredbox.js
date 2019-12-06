27b4546d9666caa3d755e6457f58aa3ddd99d683
MadreamF <sasha754@yandex.ru>
2019-12-06 11:34:26 +0300

    upd

--- coloredbox.js
+++ coloredbox.js
@@ -2,7 +2,7 @@
 	let template = document.createElement("template");
 	//template.innerHTML = "<style>:host {border-radius: 25px;border-width: 4px;border-color: black;border-style: solid;display: block;} </style> ";
 	//template.innerHTML = '<canvas id="myChart" width="400" height="400"></canvas><script>var ctx=document.getElementById("myChart"),myRadarChart=new Chart(ctx,{type:"radar",data:{labels:["Running","Swimming","Eating","Cycling"],datasets:[{data:[20,10,4,2],fill:1},{data:[28,15,2,25]}]},options:{scale:{angleLines:{display:!1},ticks:{suggestedMin:10,suggestedMax:100}}}});</script>';
-	template.innerHTML = '<div id="my_test_element">HI</div><script>console.log("shadow");</script>';
+	template.innerHTML = '<style></style>';
 	
 	class ColoredBox extends HTMLElement {
 		constructor() {
@@ -14,14 +14,16 @@
 				this.dispatchEvent(event);
 			});
 			this._props = {};
+			this._innertext = 'Initial text';
 			
-			const paragr = document.createElement('p');
+			this._paragr = document.createElement('p');
 			paragr.setAttribute('class', 'paragraph');
 			if( this.hasAttribute('innertext') ) {
 				const text = this.getAttribute('innertext');
 				paragr.textContent = text;
 			} else {
 				paragr.textContent = 'How are you?';
+				this.setAttribute('innertext', 'How are you?');
 			}
 			
 			shadowRoot.appendChild(paragr);
@@ -49,7 +51,19 @@
 				//this.setAttribute("innertext", 'Changed!');
 				console.log('innertext was in changedProperties');
 				this.style["color"] = changedProperties["innertext"];
+				this._paragr.textContent = changedProperties["innertext"];
 			}
+		}
+		
+		get innertext() {
+			return this.innertext;
+		}
+		set innertext(value) {
+			this._innertext = value;
+		}
+		
+		redraw() {
+			
 		}
 	}
 
