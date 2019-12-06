02a91a3515bc8df3c9ff58c8e050f4699ceb1ddc
MadreamF <sasha754@yandex.ru>
2019-12-06 16:06:21 +0300

    upd

--- sac-widgets/coloredbox/coloredbox.js
+++ sac-widgets/coloredbox/coloredbox.js
@@ -1,6 +1,7 @@
 (function() { 
 	let d3Script = document.createElement('script');
-	d3Script.src = 'https://d3js.org/d3.v5.min.js';
+	// d3Script.src = 'https://d3js.org/d3.v5.min.js';
+	d3Script.src = 'http://d3js.org/d3.v3.min.js';
 	d3Script.async = false;
 	document.head.appendChild(d3Script);
 
@@ -32,6 +33,9 @@
 			}
 			
 			shadowRoot.appendChild(this._paragr);
+			
+			this.$svg = document.createElement('svg');
+			shadowRoot.appendChild(this.$svg);
 		}
 		
 		// attributeChangedCallback(name, oldValue, newValue) {
@@ -66,6 +70,81 @@
 		// set innertext(value) {
 			// this._innertext = value;
 		// }
+		
+		render() {
+			var width = 960,
+				height = 500;
+
+			var nodes = d3.range(200).map(function() { return {radius: Math.random() * 12 + 4}; }),
+				root = nodes[0],
+				color = d3.scale.category10();
+			
+			root.radius = 0;
+			root.fixed = true;
+			
+			var force = d3.layout.force()
+				.gravity(0.05)
+				.charge(function(d, i) { return i ? 0 : -2000; })
+				.nodes(nodes)
+				.size([width, height]);
+			
+			force.start();
+			
+			this.$svg = d3.select("svg")
+				.attr("width", width)
+				.attr("height", height);
+				// this.$svg.setAttribute("width", width);
+				// this.$svg.setAttribute("height", height);
+			
+			this.$svg.selectAll("circle")
+				.data(nodes.slice(1))
+			.enter().append("circle")
+				.attr("r", function(d) { return d.radius; })
+				.style("fill", function(d, i) { return color(i % 3); });
+			
+			force.on("tick", function(e) {
+			var q = d3.geom.quadtree(nodes),
+				i = 0,
+				n = nodes.length;
+			
+			while (++i < n) q.visit(collide(nodes[i]));
+			
+			this.$svg.selectAll("circle")
+				.attr("cx", function(d) { return d.x; })
+				.attr("cy", function(d) { return d.y; });
+			});
+			
+			this.$svg.on("mousemove", function() {
+			var p1 = d3.mouse(this);
+			root.px = p1[0];
+			root.py = p1[1];
+			force.resume();
+			});
+		}
+		
+		collide(node) {
+			var r = node.radius + 16,
+				nx1 = node.x - r,
+				nx2 = node.x + r,
+				ny1 = node.y - r,
+				ny2 = node.y + r;
+			return function(quad, x1, y1, x2, y2) {
+				if (quad.point && (quad.point !== node)) {
+				var x = node.x - quad.point.x,
+					y = node.y - quad.point.y,
+					l = Math.sqrt(x * x + y * y),
+					r = node.radius + quad.point.radius;
+				if (l < r) {
+					l = (l - r) / l * .5;
+					node.x -= x *= l;
+					node.y -= y *= l;
+					quad.point.x += x;
+					quad.point.y += y;
+				}
+				}
+				return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+			};
+		}
 		
 	}
 
