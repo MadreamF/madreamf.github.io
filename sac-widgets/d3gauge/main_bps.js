83e419f701d1c5a15e0b0ab6f30f40eb1218b0bd
MadreamF <sasha754@yandex.ru>
2019-12-09 11:41:41 +0300

    upd

---
+++ sac-widgets/d3gauge/main_bps.js
@@ -0,0 +1,53 @@
+(function()  {
+	let template = document.createElement("template");
+	template.innerHTML = `
+		<form id="form">
+			<fieldset>
+				<legend>Value</legend>
+				<table>
+					<tr>
+						<td>Value</td>
+						<td><input id="bps_value" type="text" size="10" maxlength="10"></td>
+					</tr>
+				</table>
+				<input type="submit" style="display:none;">
+			</fieldset>
+		</form>
+		<style>
+		:host {
+			display: block;
+			padding: 1em 1em 1em 1em;
+		}
+		</style>
+	`;
+
+	class D3GaugeBps extends HTMLElement {
+		constructor() {
+			super();
+			this._shadowRoot = this.attachShadow({mode: "open"});
+			this._shadowRoot.appendChild(template.content.cloneNode(true));
+			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
+		}
+
+		_submit(e) {
+			e.preventDefault();
+			this.dispatchEvent(new CustomEvent("propertiesChanged", {
+					detail: {
+						properties: {
+							value: this.value
+						}
+					}
+			}));
+		}
+
+		set value(newValue) {
+			this._shadowRoot.getElementById("bps_value").value = newValue;
+		}
+
+		get value() {
+			return this._shadowRoot.getElementById("bps_value").value;
+		}
+	}
+
+	customElements.define("com-demo-d3gauge-bps", D3GaugeBps);
+})();
